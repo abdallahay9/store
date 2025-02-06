fa8a7fc5455891126f843d7afdfd32053ef6ad36
Abd Alhadi Ayan <AbdAlhadiAyan@gmail.com>
2025-02-06 18:22:24 +0300

    remove nav

--- src/Page/Websites/Website.js
+++ src/Page/Websites/Website.js
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 import SiteNav from "../../Components/Website/navSite/siteNav";
 export default function Website(){
     return <div style={{ background:"white" }}>
-        <SiteNav />
+        {/* <SiteNav /> */}
         <Outlet />
     </div>
 }
